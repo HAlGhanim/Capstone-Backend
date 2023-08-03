4bf50187e6b07dc98f9d8d4f85a8ef443ea5a7df
HAlGhanim <imzaji98@gmail.com>
2023-08-04 01:05:09 +0300

    sui

--- middlewares/images/multer.js
+++ middlewares/images/multer.js
@@ -9,18 +9,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  fileFilter: (req, file, cb) => {
-    if (
-      file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/jpg" ||
-      file.mimetype === "image/png"
-    ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-      return cb(new Error("Only JPG, JPEG, and PNG images are supported"));
-    }
-  },
 });
 
 module.exports = upload;
