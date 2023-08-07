d470bb0a2b4b66781af3a0dedf7f38479243b6f6
HAlGhanim <imzaji98@gmail.com>
2023-08-07 22:19:55 +0300

    changed capitalization in Event schema

--- models/Event.js
+++ models/Event.js
@@ -3,7 +3,7 @@ const { model, Schema } = require("mongoose");
 const EventSchema = new Schema({
   name: { type: String, unique: true, required: true },
   location: {
-    longtitude: { type: String, required: true },
+    longitude: { type: String, required: true },
     latitude: { type: String, required: true },
   },
   date: { type: Date, required: true },
@@ -13,7 +13,7 @@ const EventSchema = new Schema({
   price: { type: Number, required: true },
   tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
   attendees: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  Organizer: { type: Schema.Types.ObjectId, ref: "User" },
+  organizer: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
 module.exports = model("Event", EventSchema);
