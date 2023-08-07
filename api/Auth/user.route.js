0ed6b828f440b90e2175f34d75179bbd6fee6d72
Bashayer770 <Beshow95@gmail.com>
2023-08-07 19:33:56 +0300

    edited

--- api/Auth/user.route.js
+++ api/Auth/user.route.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const passport = require("passport");
 const { createUser, getUsers, signin } = require("./user.controller");
+const upload = require("../../middlewares/images/multer");
+const { hashing } = require("../../middlewares/password/password");
+const { imageConditional } = require("../../middlewares/images/pImage");
 const router = express.Router();
 
 // only for testing
 router.get("/users", getUsers);
 
-router.post("/register", createUser);
+router.post(
+  "/register",
+  upload.single("image"),
+  imageConditional,
+  hashing,
+  createUser
+);
 router.post(
   "/login",
   passport.authenticate("local", { session: false }),
